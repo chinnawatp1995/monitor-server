485df784750831f97b61cc9e46204b09e91cad4b
chinnawatp1995 <chinnawat.patt.t1995@gmail.com>
2024-11-06 16:20:28 +0700

    update : change retention policy

--- migrations/retention_policy.sql
+++ migrations/retention_policy.sql
@@ -13,16 +13,16 @@ SELECT add_retention_policy('network_usage', INTERVAL '3 months')
 -- ==========================================
 --  new for new schema
 
-SELECT add_retention_policy('request_count', INTERVAL '30 days')
+SELECT add_retention_policy('request_count', INTERVAL '90 days')
 
-SELECT add_retention_policy('response_time', INTERVAL '30 days')
+SELECT add_retention_policy('response_time', INTERVAL '90 days')
 
-SELECT add_retention_policy('error', INTERVAL '30 days')
+SELECT add_retention_policy('error', INTERVAL '90 days')
 
-SELECT add_retention_policy('cpu', INTERVAL '30 days')
+SELECT add_retention_policy('cpu', INTERVAL '90 days')
 
-SELECT add_retention_policy('mem', INTERVAL '30 days')
+SELECT add_retention_policy('mem', INTERVAL '90 days')
 
-SELECT add_retention_policy('rx_network', INTERVAL '30 days')
+SELECT add_retention_policy('rx_network', INTERVAL '90 days')
 
-SELECT add_retention_policy('tx_network', INTERVAL '30 days')
+SELECT add_retention_policy('tx_network', INTERVAL '90 days')
