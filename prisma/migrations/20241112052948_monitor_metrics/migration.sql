f6b9c0ca50cbf621d7adee4381ab59a968e41bef
chinnawatp1995 <chinnawat.patt.t1995@gmail.com>
2024-11-12 14:33:30 +0700

    update : add new command and correct sql syntax

--- prisma/migrations/20241112052948_monitor_metrics/migration.sql
+++ prisma/migrations/20241112052948_monitor_metrics/migration.sql
@@ -75,18 +75,18 @@ CREATE TABLE "tx_network" (
     "value" BIGINT NOT NULL
 );
 
--- CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION IF NOT EXISTS timescaledb;
 
--- SELECT create_hypertable('request_count', 'time');
+SELECT create_hypertable('request_count', 'time');
 
--- SELECT create_hypertable('response_time', 'time');
+SELECT create_hypertable('response_time', 'time');
 
--- SELECT create_hypertable('error', 'time');
+SELECT create_hypertable('error', 'time');
 
--- SELECT create_hypertable('cpu', 'time')
+SELECT create_hypertable('cpu', 'time');
 
--- SELECT create_hypertable('mem', 'time')
+SELECT create_hypertable('mem', 'time');
 
--- SELECT create_hypertable('rx_network', 'time')
+SELECT create_hypertable('rx_network', 'time'); 
 
--- SELECT create_hypertable('tx_network', 'time')
+SELECT create_hypertable('tx_network', 'time');
--- prisma/migrations/create_ht.sql
+++ prisma/migrations/create_ht.sql
@@ -6,10 +6,10 @@ SELECT create_hypertable('response_time', 'time');
 
 SELECT create_hypertable('error', 'time');
 
-SELECT create_hypertable('cpu', 'time')
+SELECT create_hypertable('cpu', 'time');
 
-SELECT create_hypertable('mem', 'time')
+SELECT create_hypertable('mem', 'time');
 
-SELECT create_hypertable('rx_network', 'time')
+SELECT create_hypertable('rx_network', 'time');
 
-SELECT create_hypertable('tx_network', 'time')
+SELECT create_hypertable('tx_network', 'time');
